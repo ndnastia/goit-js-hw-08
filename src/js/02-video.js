c8467e193a349e1511c929bbb65ce0bee7d2dcb5
ndnastia <ndnastiadatsiuk@gmail.com>
2023-04-11 06:54:31 +0200

    updated

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,42 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+player.on('play', function () {
+  console.log('played the video!');
+});
 
+
+
+const onPlay = function (data) {
+  // data is an object containing properties specific to that event
+    const seconds = Number(data);
+    localStorage.setItem('videoplayer-current-time', seconds);
+};
+
+player.on('play', onPlay);
+
+
+player.on("timeupdate", throttle(onPlay, 1000));
+
+const currentTime = Number(localStorage.getItem("videoplayer-current-time"));
+
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import { throttle } from "lodash";
+const form = document.querySelector('.feedback-form');
 
+form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', onSubmit);
+
+let data = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+const { email, message } = form.elements;
+reloadPage();
+
+function onInput(evt) {
+  dataForm = { email: email.value, message: message.value };
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+}
+
+function reloadPage() {
+  if (data) {
+    email.value = data.email || '';
+    message.value = data.message || '';
+  }
+}
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  console.log({ email: email.value, message: message.value });
+
+  if (!email.value === '' || !message.value === '') {
+    localStorage.removeItem('feedback-form-state');
+  evt.currentTarget.reset();
+  data = {};
+  }
+
+}
