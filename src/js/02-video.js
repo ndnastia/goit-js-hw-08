5312a241e7d697750b0fc7d4cf9c9dffc14266da
ndnastia <ndnastiadatsiuk@gmail.com>
2023-04-13 15:03:03 +0200

    updated

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,24 +1,27 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe, {
+  loop: true,
+  fullscreen: true,
+  quality: '1080p',
+});
+
 player.on('play', function () {
   console.log('played the video!');
 });
 
 
-
 const onPlay = function (data) {
-  // data is an object containing properties specific to that event
-    const seconds = Number(data);
-    localStorage.setItem('videoplayer-current-time', seconds);
+    localStorage.setItem('videoplayer-current-time', data.seconds);
 };
 
 player.on('play', onPlay);
 
+player.on('timeupdate', throttle(onPlay, 1000));
 
-player.on("timeupdate", throttle(onPlay, 1000));
-
-const currentTime = Number(localStorage.getItem("videoplayer-current-time"));
-
+const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
 
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
