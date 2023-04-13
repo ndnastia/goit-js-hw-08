fddc885463f5f87b86500e8aebe5492a33127676
ndnastia <116677721+ndnastia@users.noreply.github.com>
2023-04-13 12:03:52 +0000

    Deploying to gh-pages from @ ndnastia/goit-js-hw-08@5312a241e7d697750b0fc7d4cf9c9dffc14266da 🚀

--- 02-video.0dbba455.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),t=new Vimeo.Player(e);t.on("play",(function(){console.log("played the video!")}));var o=function(e){var t=Number(e);localStorage.setItem("videoplayer-current-time",t)};t.on("play",o),t.on("timeupdate",throttle(o,1e3));var n=Number(localStorage.getItem("videoplayer-current-time"));t.getVideoTitle().then((function(e){console.log("title:",e)})),t.setCurrentTime(n).then((function(e){})).catch((function(e){e.name}))}();
-//# sourceMappingURL=02-video.0dbba455.js.map
--- 02-video.f8d50725.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),t=new Vimeo.Player(e);t.on("play",(function(){console.log("played the video!")}));const o=function(e){const t=Number(e);localStorage.setItem("videoplayer-current-time",t)};t.on("play",o),t.on("timeupdate",throttle(o,1e3));const n=Number(localStorage.getItem("videoplayer-current-time"));t.getVideoTitle().then((function(e){console.log("title:",e)})),t.setCurrentTime(n).then((function(e){})).catch((function(e){e.name}));
-//# sourceMappingURL=02-video.f8d50725.js.map
